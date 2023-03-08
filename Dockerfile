f4f240a13581d3eb67498afa1df286a65dd904c5
lucasirustaDEV <lucasirusta@gmail.com>
2023-03-08 12:11:48 -0300

    :rocket: Add dockerfile to deploy on render

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+FROM amazoncorretto:ll-alpine-jdk
+MAINTAINER lucasirustaDEV
+COPY target/ABML-0.0.1-SNAPSHOT.jar pfap-app.jar
+ENTRYPOINT ["java","-jar","/pfap-app.jar"]
--- src/main/java/com/ABML/ABML/controller/ProyectoController.java
+++ src/main/java/com/ABML/ABML/controller/ProyectoController.java
@@ -20,8 +20,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-/*@CrossOrigin(origins = "https://portfolio-lucas-ap.web.app")*/
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://portfolio-lucas-ap.web.app")
+/*@CrossOrigin(origins = "http://localhost:4200")*/
 @RequestMapping("/proyecto")
 /*@CrossOrigin(origins = {"https://portfolio-lucas-ap.web.app","http://localhost:4200"})*/
 public class ProyectoController {
